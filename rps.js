6e3a6e7f3e0431e25cb459544a867d6a83b87479
Ashishraw-007 <rawatash9@gmail.com>
2022-06-20 21:12:49 +0530

    assign no to choice

--- rps.js
+++ rps.js
@@ -1,3 +1,12 @@
 let choice = document.querySelector('input');
 
 choice = choice.toLowerCase();
+
+let choiceInt;
+if(choice == 'rock')
+    choiceInt=1;
+else if(choice == 'paper')
+    choiceInt=2;
+else if(choice == 'scissors')
+    choiceInt=3;
+
