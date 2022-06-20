8d340d5b8bcbaca435677ad6e15ad4d57f63bba8
Ashishraw-007 <rawatash9@gmail.com>
2022-06-20 21:25:37 +0530

    add function which gives result

--- rps.js
+++ rps.js
@@ -14,3 +14,25 @@ function compInput(){
     return Math.floor(Math.random() * (4-1)+1);
 }
 
+function result(userChoice, compChoice){
+    if(userChoice == compChoice)
+        {return "draw";}
+    else if(userChoice==1){
+        if(compChoice==2)
+            return "User loses! paper beats rock";
+        else
+            return "User wins! rock beats scissors";
+    }
+    else if(userChoice==2){
+        if(compChoice==3)
+            return "User loses! scissors beats paper";
+        else
+            return "User wins! paper beats rock";
+    }
+    else if(userChoice==3){
+        if(compChoice==1)
+            return "User loses! rock beats scissors";
+        else
+            return "User wins! scissors beats paper";
+    }
+}
