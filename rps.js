5bfa194205d47e77e814b9a50d62b8eec33afd11
Ashishraw-007 <rawatash9@gmail.com>
2022-06-20 22:52:59 +0530

    add function setInput to set values

--- rps.js
+++ rps.js
@@ -1,38 +1,41 @@
-let choice = document.querySelector('input').innerText;
+let choice;
+let choiceInt; 
 
-choice = choice.toLowerCase;
+function setInput() {
+    choice = document.getElementById('choice').value;
 
-let choiceInt;
-if(choice == 'rock')
-    choiceInt=1;
-else if(choice == 'paper')
-    choiceInt=2;
-else if(choice == 'scissors')
-    choiceInt=3;
+    choice = choice.toLowerCase();
+    if (choice == 'rock')
+        choiceInt = 1;
+    else if (choice == 'paper')
+        choiceInt = 2;
+    else if (choice == 'scissors')
+        choiceInt = 3;
+}
 
-function compInput(){
-    let compInt = Math.floor(Math.random() * (4-1)+1);
-    console.log("cmputer choice- "+compInt);
+function compInput() {
+    let compInt = Math.floor(Math.random() * (4 - 1) + 1);
+    console.log("cmputer choice- " + compInt);
+    console.log(choiceInt);
     return compInt;
 }
 
-function result(userChoice, compChoice){
-    if(userChoice == compChoice)
-        {return "draw";}
-    else if(userChoice==1){
-        if(compChoice==2)
+function result(userChoice, compChoice) {
+    if (userChoice == compChoice) { return "draw"; }
+    else if (userChoice == 1) {
+        if (compChoice == 2)
             return "User loses! paper beats rock";
         else
             return "User wins! rock beats scissors";
     }
-    else if(userChoice==2){
-        if(compChoice==3)
+    else if (userChoice == 2) {
+        if (compChoice == 3)
             return "User loses! scissors beats paper";
         else
             return "User wins! paper beats rock";
     }
-    else if(userChoice==3){
-        if(compChoice==1)
+    else if (userChoice == 3) {
+        if (compChoice == 1)
             return "User loses! rock beats scissors";
         else
             return "User wins! scissors beats paper";
@@ -41,6 +44,7 @@ function result(userChoice, compChoice){
 
 let submitBtn = document.querySelector('button');
 
-submitBtn.addEventListener('click',()=>{
-    console.log(result(choiceInt,compInput())); 
+submitBtn.addEventListener('click', () => {
+    setInput();
+    console.log(result(choiceInt, compInput()));
 })
